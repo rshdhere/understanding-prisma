2668149fb9a2643905f02f9e21a8b40a1668a5cb
raashed <mohammedraashed10@gmail.com>
2025-08-20 14:30:54 +0530

    chore: resolved eslint and prettier in-future conflicts

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,14 +1,16 @@
 import js from "@eslint/js";
 import globals from "globals";
 import tseslint from "typescript-eslint";
-import { defineConfig } from "eslint/config";
+import eslintConfigPrettier from "eslint-config-prettier";
 
-export default defineConfig([
+export default [
+  js.configs.recommended,
+  ...tseslint.configs.recommended,
+  eslintConfigPrettier,
   {
     files: ["**/*.{js,mjs,cjs,ts,mts,cts}"],
-    plugins: { js },
-    extends: ["js/recommended"],
-    languageOptions: { globals: globals.node },
+    languageOptions: {
+      globals: globals.node,
+    },
   },
-  tseslint.configs.recommended,
-]);
+];
